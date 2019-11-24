c9e4ccd1db512d3496d28b7f4484c78afe8ac758
patrickett <patrickett@protonmail.com>
2019-11-24 15:50:36 -0500

    cleaned config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,14 +8,11 @@ alias lt="exa --tree"
 alias uninstall="sudo pacman -R"
 alias purge="sudo pacman -Rs"
 alias e="exit"
-alias cls=clear
 
 #programs
 alias n="nano"
 alias music="ncmpcpp"
 alias monitor="sudo airmon-ng start"
-alias py2=python2
-alias py3=python3
 alias search="sudo find -mindepth 0 -maxdepth 10000 -name "
 
 #program commands
@@ -27,33 +24,30 @@ alias scrape="youtube-dl"
 alias aur="sh ~/git-dev/scripts/bash/aurinstall"
 alias ranger="ranger-cd"
 
-
 #directories
 alias des="cd Desktop ; ls -1"
 alias down="cd Downloads ; ls -1"
-alias con="cd .config ; ls -1"  
+alias con="cd .config ; ls -1"
 alias doc="cd Documents; ls -1"
 alias pyp="cd Development/PythonProjects/ ; ls -1"
 alias cppp="cd Development/C++Projects/; ls -1"
 
 
-function ranger-cd                                                               
+function ranger-cd
+  set tempfile '/tmp/chosendir'
+  /usr/bin/ranger --choosedir=$tempfile (pwd)
 
-  set tempfile '/tmp/chosendir'                                                  
-  /usr/bin/ranger --choosedir=$tempfile (pwd)                                    
-
-  if test -f $tempfile                                                           
-      if [ (cat $tempfile) != (pwd) ]                                            
-        cd (cat $tempfile)                                                       
-      end                                                                        
-  end                                                                            
-
-  rm -f $tempfile                                                                
-
-end                                                                              
-
-function fish_user_key_bindings                                                  
-    bind \co 'ranger-cd ; fish_prompt'                                           
+  if test -f $tempfile
+      if [ (cat $tempfile) != (pwd) ]
+        cd (cat $tempfile)
+      end
+  end
+  rm -f $tempfile
 end
 
+function fish_user_key_bindings
+    bind \co 'ranger-cd ; fish_prompt'
+end
+
+# binds Ctrl+R in the shell to open ranger
 bind \cr 'ranger; clear; exa -1; commandline -f repaint'
